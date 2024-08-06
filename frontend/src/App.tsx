30dc033f44d287c07f93653f236ea7e662ae83b0
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-06 13:08:22 -0600

    Added header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import TodoPage from "./pages/TodoPage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <BrowserRouter>
+        <Header
+            brand={{
+                title: "X App",
+                url: "/"
+            }}
+            links={[
+                {
+                    title: "Home",
+                    url: "/"
+                }
+            ]}
+        />
+        <Routes>
+            <Route path={"/"} element={<TodoPage/>}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
