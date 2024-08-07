866c2789fcf8478a918658f14acc632292f4a434
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-07 16:10:24 -0600

    Added get and post todo methods

--- backend/Controllers/TodoController.cs
+++ backend/Controllers/TodoController.cs
@@ -17,8 +17,27 @@ public class TodoController : ControllerBase
     }
     
     [HttpGet]
-    public async Task<IEnumerable<Todo>> GetTodos()
+    public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
     {
         return await _context.Todos.ToListAsync();
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Todo>> GetTodo(int id)
+    {
+        var todo = await _context.Todos.FindAsync(id);
+        if(todo == null)
+        {
+            return NotFound();
+        }
+        return todo;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<Todo>> PostTodo(Todo todo)
+    {
+        _context.Todos.Add(todo);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction("GetTodo", new { id = todo.id }, todo);
     }
 }
