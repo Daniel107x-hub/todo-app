844a0934b951a202087cc0735edbfbf6881e89e5
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-16 09:31:47 -0600

    Auth: Added login styles and redirection on login page if already authenticated

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,10 +1,12 @@
 import { GoogleLogin } from "@react-oauth/google";
 import {setUser, useLoginMutation} from "../../redux/Auth/AuthSlice";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import {Navigate, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 import { setTokenInLocalStorage } from "../../utils/LocalStorageUtils";
 import {toast} from "react-toastify";
 import {User} from "../../types/Todo";
+import {Card} from "react-bootstrap";
+import styles from "./LoginPage.module.css";
 
 export type AuthResponse = {
     credential: string;
@@ -21,6 +23,7 @@ const LoginPage = () => {
     const [executeLogin, result] = useLoginMutation<AuthResponse>();
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const user = useSelector((state: any) => state.auth.user);
     const onSuccess = async (response:any) => {
         const data = (await executeLogin(response.credential)).data as LoginResponse;
         const token = data?.token;
@@ -38,13 +41,24 @@ const LoginPage = () => {
         console.log('Login Failed:');
     };
 
+    if(user) return <Navigate to={"/"}/>
+
     return (
-    <div>
-        <h2>Login with Google</h2>
-        <GoogleLogin
-            onSuccess={onSuccess}
-            onError={onFailure}
-        />
+    <div className={styles.content}>
+        <Card className={styles.card}>
+            <h2>Login</h2>
+            <div className={styles.loginOptions}>
+                <span className={styles.option}>
+                    <h4>Login with google</h4>
+                    <GoogleLogin
+                        shape="pill"
+                        theme="filled_blue"
+                        onSuccess={onSuccess}
+                        onError={onFailure}
+                    />
+                </span>
+            </div>
+        </Card>
     </div>
     );
 };
