6a0043dd6809d77893d35690ae9e0dae7e1a052c
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-20 14:10:14 -0600

    Added missing migrations

---
+++ backend/Migrations/20240819200418_AddedUserTodoRelation.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserTodoRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "picture",
+                table: "Users",
+                newName: "Picture");
+
+            migrationBuilder.RenameColumn(
+                name: "lastName",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "firstName",
+                table: "Users",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "dateOfBirth",
+                table: "Users",
+                newName: "DateOfBirth");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "title",
+                table: "Todos",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Todos",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "completed",
+                table: "Todos",
+                newName: "Completed");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Todos",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "Todos",
+                type: "character varying(50)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_UserEmail",
+                table: "Todos",
+                column: "UserEmail");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Users_UserEmail",
+                table: "Todos",
+                column: "UserEmail",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Users_UserEmail",
+                table: "Todos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Todos_UserEmail",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "Todos");
+
+            migrationBuilder.RenameColumn(
+                name: "Picture",
+                table: "Users",
+                newName: "picture");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "lastName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Users",
+                newName: "firstName");
+
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Users",
+                newName: "dateOfBirth");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Todos",
+                newName: "title");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Todos",
+                newName: "description");
+
+            migrationBuilder.RenameColumn(
+                name: "Completed",
+                table: "Todos",
+                newName: "completed");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Todos",
+                newName: "id");
+        }
+    }
+}
---
+++ backend/Migrations/20240819200539_AddedCascadeBehaviorOnUserDelete.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCascadeBehaviorOnUserDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Users_UserEmail",
+                table: "Todos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Users_UserEmail",
+                table: "Todos",
+                column: "UserEmail",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Users_UserEmail",
+                table: "Todos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Users_UserEmail",
+                table: "Todos",
+                column: "UserEmail",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
