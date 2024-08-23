e20e4b150ba889887a8dd8c5bb05c4957b3bd6d3
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-22 18:24:44 -0600

    Added docker files for containerization

--- frontend/src/client/axiosInstance.ts
+++ frontend/src/client/axiosInstance.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import {API_BASE_PATH} from "../constants/constants";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5299' + API_BASE_PATH
+    baseURL: 'https://localhost:3001' + API_BASE_PATH
 });
 export default axiosInstance;
